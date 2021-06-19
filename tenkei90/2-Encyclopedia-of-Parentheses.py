97c69f39f4af1db7222da8be31c61a67f82afd92
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-06-20 04:44:23 +0900

    Add files via upload

---
+++ tenkei90/2-Encyclopedia-of-Parentheses.py
@@ -0,0 +1,44 @@
+#2  Encyclopedia of Parentheses（★3）AC
+N=int(input())
+if N%2==0:
+    n=N//2
+    S="("*n + ")"*n
+    S=list(S)
+    S_end="()"*n
+    S_end=list(S_end)
+    
+    pos=[i for i in range(n)]
+    #print(pos)
+    
+    if N!=2:
+        while not (S==S_end):
+            S=")"*N
+            S=list(S)
+            #print(pos)
+            for x in pos:
+                S[x]="("
+            print("".join(map(str,S)))
+            #print(pos)
+            c=0
+            i=n-1
+            while c==0:
+                if pos[i]!=(i*2):
+                    if i==n-1:
+                        pos[i]+=1
+                        c=1
+                    elif pos[i]!=(pos[i+1]-1):
+                        pos[i]+=1
+                        c=1
+                    else:
+                        c=1
+                else:
+                    i-=1
+                    if pos[i]!=(i*2):
+                        pos[i:]=[(pos[i]+j+1) for j in range(n-i)]
+                        c=1
+                        #print("a"+str(pos))
+                    
+                    
+                    
+    elif N==2:
+        print("()")
