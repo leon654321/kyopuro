44cbc776176092b9643116a9dd1f5db5d146d9cf
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-06-20 05:03:01 +0900

    Create 024_Select+／-One.py

---
+++ tenkei90/024_Select+／-One.py
@@ -0,0 +1,10 @@
+N,K=map(int,input().split())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+C=[abs(A[i]-B[i]) for i in range(N)]
+s=sum(C)
+
+if K>=s and(K-s)%2==0:
+    print("Yes")
+else:
+    print("No")
