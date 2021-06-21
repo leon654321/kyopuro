e955621086b78ff3a8ccd50cea909e50b3f3d372
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-06-21 23:19:05 +0900

    Update 010_Score-Sum-Queries.py

--- tenkei90/010_Score-Sum-Queries.py
+++ tenkei90/010_Score-Sum-Queries.py
@@ -1,3 +1,8 @@
+#問題文
+#https://atcoder.jp/contests/typical90/tasks/typical90_j
+
+#累計和的な解き方だがこのころは競プロ初心者のため作りがガタガタ
+
 import sys
 myinput=input#sys.stdin.readline
  
@@ -7,16 +12,16 @@ def func(n,cl,c_all):
     for i in range(1,n+1):
         c,p=map(int,myinput().split())
         s1+=p
-        cl[i]=s1
+        cl[i]=s1 #全生徒の累計和
         if c==1:
             s2+=p
-        c_all[i]=s2    
+        c_all[i]=s2    #クラス1の生徒の番号iまでの累計和
         
 def func2(q,cc_all,cc):
     for _ in range(q):
         L,R=map(int,myinput().split())
-        c1=cc_all[R]-cc_all[L-1]
-        c2=cc[R]-cc[L-1]-c1
+        c1=cc_all[R]-cc_all[L-1] #クラス1の生徒の番号iまでの累計和から算出
+        c2=cc[R]-cc[L-1]-c1 #LからRまでの全生徒の累計和からc1を引く
         print(c1,c2)        
 
 N=int(myinput())
