c5731652c105816bc4ce752d49a89999bf1d0b20
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-06-24 21:48:03 +0900

    Update 013_Passing.py

--- tenkei90/013_Passing.py
+++ tenkei90/013_Passing.py
@@ -1,3 +1,6 @@
+#問題文
+#https://atcoder.jp/contests/typical90/tasks/typical90_m
+
 from collections import deque,defaultdict
 import sys
 input=sys.stdin.readline
@@ -10,7 +13,9 @@ def main():
         G[a-1].append((b-1,c))
         G[b-1].append((a-1,c))
 
-
+    #dilkstraで解ける
+    
+    #街 k+1 から各街へ行くときにかかる最小コスト
     def func(k):
         d=defaultdict(lambda:10**9+1)
         d[k]=0
@@ -28,10 +33,15 @@ def main():
                 qapp((nxt,new_dist))
 
         return d
-
+    
+    #街 1 から各点へ行くときにかかる最小コスト
     D0=func(0)
+    #街 N から各点へ行くときにかかる最小コスト
     D1=func(N-1)
-
+    
+    #街 1 から街 i+1 を経由して街 N まで移動するときにかかるコスト
+    #　＝（街 1 から街 i+1へ行くときにかかる最小コスト）
+    #     ＋（街 N から街 i+1へ行くときにかかる最小コスト）
     for i in range(N):
         print(D0[i]+D1[i])
 
