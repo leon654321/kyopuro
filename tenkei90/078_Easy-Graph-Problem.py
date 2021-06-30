0065547dc19fcf18d69bdbab2d1e360ee3a3e2c1
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-06-30 23:32:49 +0900

    Update 078_Easy-Graph-Problem.py

--- tenkei90/078_Easy-Graph-Problem.py
+++ tenkei90/078_Easy-Graph-Problem.py
@@ -10,8 +10,10 @@ G=[[]for _ in range(N)]
 ans=0 #答え
 
 for _ in range(M):
+    #入力
     a,b=map(int,input().split())
     if a<b:
+        
         #相互にくっつけても時間を取るだけなので片方だけ
         G[b-1].append(a-1)
         
@@ -23,6 +25,7 @@ for _ in range(M):
             ans-=1
     else:
         G[a-1].append(b-1)
+        
         if len(G[a-1])==1:
             ans+=1
         elif len(G[a-1])==2:
