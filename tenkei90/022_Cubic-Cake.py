11200eba1e089672c7578b07d323665c84942773
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-07-01 23:31:16 +0900

    Update 022_Cubic-Cake.py

--- tenkei90/022_Cubic-Cake.py
+++ tenkei90/022_Cubic-Cake.py
@@ -2,6 +2,7 @@ import sys
 sys.setrecursionlimit((10 ** 9))  # 変更
 from functools import lru_cache
 
+#最大公約数の関数
 @lru_cache(maxsize=None)
 def eucrid(x,y):
     if y>x:
@@ -11,9 +12,14 @@ def eucrid(x,y):
         return y
     else:
         return eucrid(y,z)
-        
+     
+#入力
 a,b,c=map(int,input().split())
+
+#a,b,cの最大公約数
 m=eucrid(a,b)
 m=eucrid(m,c)
+
+#ケーキを切る回数
 ans=(a//m-1)+(b//m-1)+(c//m-1)
 print(ans)
