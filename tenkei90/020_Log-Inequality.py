55b5842d6d68a8637d7218e811513d8a20913302
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-07-01 23:16:47 +0900

    Update 020_Log-Inequality.py

--- tenkei90/020_Log-Inequality.py
+++ tenkei90/020_Log-Inequality.py
@@ -1,4 +1,10 @@
+#問題文
+#https://atcoder.jp/contests/typical90/tasks/typical90_t
+
+#入力
 a,b,c=map(int,input().split())
+
+#2の指数に乗せれば2^(log2a)=a, 2^(log2c^b)=c^bなのでそれを比較すればよい
 if a<c**b:
     print("Yes")
 else:
