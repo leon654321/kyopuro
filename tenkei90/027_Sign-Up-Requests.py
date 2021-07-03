f3695230e30beeac0eaffdfd9cca7465e60ac7b0
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-07-03 23:40:17 +0900

    Update 027_Sign-Up-Requests.py

--- tenkei90/027_Sign-Up-Requests.py
+++ tenkei90/027_Sign-Up-Requests.py
@@ -1,9 +1,18 @@
+#問題文
+#https://atcoder.jp/contests/typical90/tasks/typical90_aa
+
 import sys
 myinput=sys.stdin.readline
+
 N=int(myinput())
+
+#既にいるユーザー名管理
 ls=set([])
+
 for i in range(1,N+1):
     x=myinput()
+    
+    #すでにユーザー名が登録されているか判定
     if x in ls:
         continue
     else:
