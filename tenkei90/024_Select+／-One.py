8f0cec3ef8b1e47b2f90f4fbf061f17d6f79f81d
leon654321 <86167241+leon654321@users.noreply.github.com>
2021-07-03 23:29:51 +0900

    Update 024_Select+／-One.py

--- tenkei90/024_Select+／-One.py
+++ tenkei90/024_Select+／-One.py
@@ -1,14 +1,20 @@
-#
-#
+#問題文
+#https://atcoder.jp/contests/typical90/tasks/typical90_x
 
-#
+#入力
 N,K=map(int,input().split())
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
+
+#AとBの差の絶対値
 C=[abs(A[i]-B[i]) for i in range(N)]
+
+#差の絶対値の和
 s=sum(C)
+
 
 if K>=s and(K-s)%2==0:
     print("Yes")
+#たとえK>=sでも差が奇数なら0にできない
 else:
     print("No")
